e5e17bd6ecd47f7c595c45533c1160f1f9aefedb
suriydi <suriydi@gmail.com>
2024-08-15 11:47:34 +0530

    Initial commit

---
+++ src/test/java/com/suri/__SpringBootKafka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suri.__SpringBootKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
