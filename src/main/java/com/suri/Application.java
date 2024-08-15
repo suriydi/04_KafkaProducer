e1e7959e60f35c7bc14459e0707488b0a4dacbfc
suriydi <suriydi@gmail.com>
2024-08-15 12:30:42 +0530

    Initial Commit2!

--- src/main/java/com/suri/Application.java
+++ src/main/java/com/suri/Application.java
@@ -1,4 +1,4 @@
-package com.suri.__SpringBootKafka;
+package com.suri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Welcome Spring Boot Setup Sucess");
 	}
 
 }
